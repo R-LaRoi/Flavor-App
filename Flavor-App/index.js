7cc2c6dfd0964ff5ca915c9de97674a5064c5291
R. LaRoi <are@raewhoo.com>
2023-04-14 17:08:43 +0200

    Render Order Page

--- Flavor-App/index.js
+++ Flavor-App/index.js
@@ -2,7 +2,7 @@ const flavorsMenu = [
   {
     name: "Single",
     description: ["one scoop"],
-    id: 0,
+    id: 1000,
     price: 4.75,
     flavors: ["pumpkin pie", "brownie", "salted caramel"],
     emoji: "🍦",
@@ -13,7 +13,7 @@ const flavorsMenu = [
     price: 8.5,
     flavors: ["pumpkin pie", "brownie", "salted caramel"],
     emoji: "🍨",
-    id: 1,
+    id: 2000,
   },
 
   {
@@ -22,20 +22,63 @@ const flavorsMenu = [
     price: 12,
     flavors: ["pumpkin pie", "brownie", "salted caramel"],
     emoji: "🥳",
-    id: 2,
+    id: 3000,
   },
 ];
 
-const showMenu = document.getElementById("select-menu");
+let itemsAdded = [];
 
+let createCart = document.getElementById("add-items");
+const showMenu = document.getElementById("select-menu");
 const showFlavors = flavorsMenu.map(
   (flavor) =>
-    `<ul>
+    `
+    <div class="order-options">
+    <ul>
     <li class="name">${flavor.name}</li>
     <li class="description">${flavor.description}</li>
     <li class="flavors">${flavor.flavors}</li>
     <li class="price">${flavor.price}</li>
-  </ul>`
+   
+<button class="add-btn" id="add-btn" data-add="${flavor.id}">+</button>
+  </ul>
+   <hr >
+  </div
+  `
 );
 
 showMenu.innerHTML = showFlavors;
+
+document.addEventListener("click", function (event) {
+  if (event.target.dataset.add) {
+    createOrder(event.target.dataset.add);
+  }
+});
+
+function createOrder(flavorId) {
+  const addItem = flavorsMenu.filter(function (item) {
+    return item.id == flavorId;
+  })[0];
+
+  if (itemsAdded) {
+    itemsAdded.push(addItem);
+
+    itemsAdded.forEach(function (addItem) {
+      createCart.innerHTML += `
+          <div class="cart-items" id="add-to-cart">
+            ${addItem.emoji} ${addItem.name} $${addItem.price}
+          </div>
+       `;
+      console.log(addItem.name);
+    });
+  }
+
+  showCartItems = `
+  <div>Your Order</div>
+  <div class="cart-items" id="add-to-cart"> ${addItem.emoji} ${addItem.name} $${addItem.price}
+  </div>
+  <button class="purchase-btn">Complete Order</button>`;
+
+  // console.log(itemsAdded);
+  createCart.innerHTML = showCartItems;
+}
